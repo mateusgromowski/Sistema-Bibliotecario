3662b100f234a6174b5664b6bdd1ed7c2850d262
Mateus Barbosa Gromowski <mateusbarbosagromowski@gmail.com>
2025-08-30 21:41:54 -0300

    Adicionadas classes Main e Menus

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Menus menus = new Menus();
+		String mainMenuChoice = ":P";
+		while (!mainMenuChoice.equals("0")) {
+			System.out.println("----------SISTEMA BIBLIOTECÁRIO----------");
+			menus.showMenus();
+			System.out.printf("Sua escolha: ");
+			mainMenuChoice = sc.nextLine();
+
+			switch (mainMenuChoice) {
+				case "1":
+					System.out.println("---------------MENU LIVROS---------------");
+					menus.booksMenu();
+					System.out.print("Sua escolha: ");
+					sc.nextLine();		
+					break;
+				case "2":
+					System.out.println("---------------MENU USUÁRIOS---------------");
+					menus.usersMenu();
+					System.out.print("Sua escolha: ");
+					sc.nextLine();	
+					break;
+				case "3":
+					System.out.println("-------------MENU EMPRÉSTIMOS-------------");
+					menus.borrowsMenu();
+					System.out.print("Sua escolha: ");
+					sc.nextLine();
+					break;
+				case "0":
+					break;
+				default:
+					System.out.println("Entrada Inválida.");
+			}
+		}
+		System.out.println("Desligando o sistema...");
+		sc.close();
+	}
+}
---
+++ Menus.java
@@ -0,0 +1,32 @@
+public class Menus {
+	void showMenus() {
+		System.out.println("Qual função você deseja acessar?: ");
+		System.out.println("1 - Gerenciar Livros");
+		System.out.println("2 - Gerenciar Usuários");
+		System.out.println("3 - Empréstimo De Livros");
+		System.out.println("0 - Sair");
+	}
+	
+	void booksMenu() {
+		System.out.println("Qual tarefa você deseja fazer?");
+		System.out.println("1 - Adicionar Livro");
+		System.out.println("2 - Listar Livros");
+		System.out.println("3 - Buscar Livro");
+		System.out.println("0 - Sair");
+	}
+	
+	void usersMenu() {	
+		System.out.println("Qual tarefa você deseja fazer?");
+		System.out.println("1 - Adicionar Usuário");
+		System.out.println("2 - Listar Usuários");
+		System.out.println("0 - Sair");	 		
+	}
+	
+	void borrowsMenu() {
+		System.out.println("Qual tarefa você deseja fazer?");
+		System.out.println("1 - Registrar empréstimo de livro");
+		System.out.println("2 - Registrar devolução");
+		System.out.println("3 - Mostrar livros emprestados e disponíveis");
+		System.out.println("0 - Sair");							
+	}				
+}
