16e9a70bc73611c2b0e6eef25284d0e55623599c
Mateus Gromowski <mateusbarbosagromowski@gmail.com>
2026-06-01 14:24:50 -0300

    feat: implementada funcionalidade de adição de empréstimo na ui

--- sistemabibliotecario/src/main/java/com/mateusgromowski/ui/LoanMenu.java
+++ sistemabibliotecario/src/main/java/com/mateusgromowski/ui/LoanMenu.java
@@ -14,7 +14,40 @@ public class LoanMenu {
     }
 
     public void showMenu() {
-        
+        int input = 0;
+        do {
+            System.out.println("\n=== MENU DE EMPRÉSTIMOS ===");
+            System.out.println("1. Criar empréstimo");
+            System.out.println("2. Buscar empréstimo");
+            System.out.println("3. Atualizar empréstimo");
+            System.out.println("4. Devolver livro");
+            System.out.println("5. Deletar empréstimo");
+            System.out.println("0. Voltar");
+            System.out.print("Escolha: ");
+            input = Integer.parseInt(sc.nextLine());
+            loanMenu(input);
+        } while (input != 0);
+    }
+
+    private void loanMenu(int input) {
+        switch (input) {
+            case 1:
+                addLoan();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void addLoan() {
+        System.out.print("Insira o id do livro: ");
+        int bookId = Integer.parseInt(sc.nextLine());
+        System.out.print("Insira o id do usuário: ");
+
+        int userId = Integer.parseInt(sc.nextLine());
+        controller.addLoan(bookId, userId);
+        System.out.println("Livro emprestado com sucesso.");
     }
 
 }
